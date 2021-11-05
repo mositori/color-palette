a5e69e9f10a3c4e4e4b4d4da43495f296d28ed7c
mositori <destrage1958@gmail.com>
2021-11-05 22:01:55 +0900

    Mono

---
+++ index.ts
@@ -0,0 +1,14 @@
+const Mono = {
+    Mono50: "#222222",  // 34
+    Mono100: "#2E2E2E", // 46
+    Mono200: "#484848", // 72
+    Mono300: "#5C5C5C", // 92
+    Mono400: "#6E6E6E", // 110
+    Mono500: "#8C8C8C", // 140
+    Mono600: "#AAAAAA", // 170
+    Mono700: "#D1D1D1", // 209
+    Mono800: "#E2E2E2", // 229
+    Mono900: "#FFFFFF"  // 255
+} as const
+
+export default Mono
