7c19997212019ff7616d61ab4e9f498ef59c5a27
mositori <destrage1958@gmail.com>
2021-11-06 20:30:23 +0900

    面倒なのでdistも上げる

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Mono = void 0;
+exports.Mono = {
+    Mono50: "#222222",
+    Mono100: "#2E2E2E",
+    Mono200: "#484848",
+    Mono300: "#5C5C5C",
+    Mono400: "#6E6E6E",
+    Mono500: "#8C8C8C",
+    Mono600: "#AAAAAA",
+    Mono700: "#D1D1D1",
+    Mono800: "#E2E2E2",
+    Mono900: "#FFFFFF"
+};
